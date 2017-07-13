03a7aee21064a36361b598df4d9315bf5595e0ad
tjallen27 <tjallen91@hotmail.co.uk>
2017-07-13 10:08:31 +0100

    Stripe views added

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static#about'
 
   resources :tracks
-  resources :tracks
   resources :categories
+  resources :charges
 
   root 'static#home'
 
